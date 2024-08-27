8e5b8e3489dcf2e97843e60e4c7418c62b558ce5
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-08-27 13:31:42 -0700

    [ADD] agregada la conexion con la base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from "express";
 import router from './router'
+import db from "./config/db";
 
+//Conexion a base de datos
+const connectDB = async () => {
+    try{
+        db.authenticate()
+        db.sync()
+        console.log('Conexion exitosa a la base de datos')
+    } catch(error) {
+        console.error(error)
+        console.log('Hubo un error al conectar a la base de datos')
+    }
+}
+connectDB()
 const server = express()
 server.use('/api/products', router)
 
