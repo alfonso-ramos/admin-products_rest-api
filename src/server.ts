e7649e7f018684fcdc98eff7296f3d063483aff3
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-08-29 09:09:21 -0700

    [ADD] agregando documentacion con swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import colors from 'colors'
+import swaggerUi from "swagger-ui-express";
 import router from './router'
 import db from "./config/db";
+import swaggerSpec from "./config/swagger";
 
 //Conexion a base de datos
 const connectDB = async () => {
@@ -26,5 +28,10 @@ server.use('/api/products', router)
 server.get('/api', (req,res) => {
     res.json({msg: 'Desde API'})
 })
+
+//Docs
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+
 
 export default server
